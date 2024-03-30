b65db7c43ff65130c222f91b0c9976b28d17b767
EkaterinaGladkikh <103758749+EkaterinaGladkikh@users.noreply.github.com>
2024-03-30 23:57:19 +0300

    Update variables.py

--- variables.py
+++ variables.py
@@ -3,5 +3,5 @@ DATABASE = 'AdventureWorks2012'
 USERNAME = 'UserForRobotFR'
 PASSWORD = 'robotframework'
 ENCRYPT = 'no'
-connectionString = f'DRIVER={{/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.0.so.1.1}};SERVER={SERVER};DATABASE={DATABASE};' \
+connectionString = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};' \
                    f'UID={USERNAME};PWD={PASSWORD};ENCRYPT={ENCRYPT}'
