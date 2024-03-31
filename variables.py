6b3cbf3b795dbfa092c4789a39f084d05136851f
EkaterinaGladkikh <103758749+EkaterinaGladkikh@users.noreply.github.com>
2024-03-31 12:12:38 +0300

    Update variables.py

--- variables.py
+++ variables.py
@@ -2,6 +2,7 @@ server = 'LAPTOP-6BFCM7D0\SQLEXPRESS'
 database_name = 'AdventureWorks2012'
 username = 'UserForRobotFR'
 password = 'robotframework'
+port = '1433'
 ENCRYPT = 'no'
 
 connectionString = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database_name};' \
